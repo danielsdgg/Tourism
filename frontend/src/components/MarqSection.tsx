56e4e0a3a1ca4c6d9aaa133b9b2218b6f0f5e12f
Daniel Muiruri <gathigidg26@gmail.com>
2024-07-24 11:13:45 +0300

    :sparkles: Adding new features on client side

---
+++ frontend/src/components/MarqSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import '../assets/Jubilee_Insurance.jpg';
+import '../assets/NCBA.jpg';
+import '../assets/coop.jpg';
+import '../assets/grizzly.png';
+import '../assets/saf.png';
+
+const MarqSection = () => {
+  return (
+    <div className='app'>
+
+    </div>
+  )
+}
+
+export default MarqSection
